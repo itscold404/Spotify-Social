f1e4cfecfe42bafaea798e742e213a78a7c1829c
itscold404 <81492047+itscold404@users.noreply.github.com>
2023-10-29 13:45:55 -0400

    database password

--- important_info/database stuff/spotify_social_database2023-10-26 (23_09_09).sql
+++ important_info/database stuff/spotify_social_database2023-10-26 (23_09_09).sql
@@ -19,9 +19,9 @@
 -- Current Database: `spotifySocial`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `spotifySocial` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `spotifySocial_dummy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `spotifySocial`;
+USE `spotifySocial_dummy`;
 
 --
 -- Table structure for table `album`
