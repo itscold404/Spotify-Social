b3ed6b41569c83dc6c0be42911ee03f44b71acef
itscold404 <81492047+itscold404@users.noreply.github.com>
2023-11-01 19:55:15 -0400

    handles both remote and local database

--- spotify_social/utils.py
+++ spotify_social/utils.py
@@ -1,14 +1,16 @@
+import os
 import MySQLdb
 
 def connect_to_db():
-    # TODO: when deploying website, uncomment remote host and comment local host
+    if os.getenv('GAE_APPLICATION', None):
+        host_val = '/cloudsql/spotify-social-media:us-east4:spotify-social-media'
+    else:
+        # Running locally
+        host_val = '127.0.0.1'
+        
     return MySQLdb.connect(
-        # uncomment host below for hosting remotely
-        # host='/cloudsql/spotify-social-media:us-east4:spotify-social-media',
-        # uncomment the host below for hosting locally 
-        host='127.0.0.1', 
+        host=host_val,
         user='david',
         passwd='r00tp@ssword',
         db='spotifySocial_dummy'
     )
-    
